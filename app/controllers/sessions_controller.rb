ea94457b5352769e0100b295991cee56da14c848
Kelsha_Darby <ohanaipo@gmail.com>
2020-05-29 18:33:59 -0600

    Create user in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    request.env["omniauth.auth"]["uid"]
-    request.env["omniauth.auth"]["info"]["first_name"]
-    request.env["omniauth.auth"]["info"]["last_name"]
-    request.env["omniauth.auth"]["info"]["email"]
+    User.create({
+      uid: request.env["omniauth.auth"]["uid"],
+      first_name: request.env["omniauth.auth"]["info"]["first_name"],
+      last_name: request.env["omniauth.auth"]["info"]["last_name"],
+      email: request.env["omniauth.auth"]["info"]["email"]
+    })
+    redirect_to '/'
   end
 end
