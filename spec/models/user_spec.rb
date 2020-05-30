5be1eaacdd48ee23771871d6f9ae2b9cb02eff87
Kelsha_Darby <ohanaipo@gmail.com>
2020-05-29 18:33:36 -0600

    Add attributes to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { should validate_presence_of :uid }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+  end
 end
