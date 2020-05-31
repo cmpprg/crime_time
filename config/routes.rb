2e98a8ae30a3a3ee84d3e21ecf9c7c15708f7c4f
Jordan Sewell <jrsewell400@gmail.com>
2020-05-30 20:54:12 -0600

    Add delete route for session destroy controller aciton

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/auth/google_oauth2', as: 'google_login'
   get '/auth/google_oauth2/callback', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
 
   namespace :user do
     get '/dashboard', to: 'dashboard#show'
