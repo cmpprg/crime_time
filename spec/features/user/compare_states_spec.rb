c6ce28f5ce3a5c12cfb0bb860527b587c0a3ea25
Kelsha_Darby <ohanaipo@gmail.com>
2020-05-31 17:10:48 -0600

    Add edge case testing for comparing the same states

--- spec/features/user/compare_states_spec.rb
+++ spec/features/user/compare_states_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "As a User" do
     @user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
+
   it "I cannot see the compare button in the nav bar" do
     visit user_compare_path
 
@@ -12,6 +13,24 @@ RSpec.describe "As a User" do
       expect(page).to have_no_link("Compare")
     end
   end
+
+  it "I cannot select the same state for state 1 and 2" do
+    visit user_compare_path
+
+    within ('.state-1') do
+      select "Colorado", from: :state
+    end
+
+    within ('.state-2') do
+      select "Colorado", from: :state
+    end
+
+    click_on "Compare"
+
+    expect(current_path).to eq(user_compare_path)
+    expect(page).to have_content("You cannot compare the same state. Please select two different states.")
+  end
+
   it "I can compare stats from 2 states" do
     visit user_compare_path
 
