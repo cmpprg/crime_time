43de721da26ceb6c3335a649b9b57e468f1dab12
Kelsha_Darby <ohanaipo@gmail.com>
2020-05-31 17:13:35 -0600

    Add compare flash to restrict user from comparing the same state

---
+++ app/assets/javascripts/compare_show.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let button = document.querySelector('#compare-button');
+  let state1 = document.querySelector("#state1")
+  let state2 = document.querySelector("#state2")
+  let flash = document.querySelector("#js-flash")
+
+  button.addEventListener('click', function() {
+    if(state1.value == state2.value){
+      flash.innerHTML = "You cannot compare the same state. Please select two different states."
+    }
+  });
+});
