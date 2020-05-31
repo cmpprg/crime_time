86c99213525950f85939811791e8e9a4f4a93458
Jenny Klich <jennyklich@gmail.com>
2020-05-31 13:14:30 -0600

    Add test and functionality so a user can compare stats for 2 states

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user,
+                :compare_path?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def compare_path?
+    request.path == "/user/compare"
   end
 end
---
+++ spec/features/user/compare_states_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe "As a User" do
+  before :each do
+    @user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+  it "I cannot see the compare button in the nav bar" do
+    visit user_compare_path
+
+    within 'nav' do
+      expect(page).to have_no_link("Compare")
+    end
+  end
+  it "I can compare stats from 2 states" do
+    visit user_compare_path
+
+    within ('.state-1') do
+      expect(page).to have_select('state')
+      select "Colorado", from: :state
+      expect(find('.state').value).to eq("CO")
+    end
+
+    within ('.state-2') do
+      expect(page).to have_select('state')
+      select "Texas", from: :state
+      expect(find('.state').value).to eq("TX")
+    end
+
+    click_on "Compare"
+
+    expect(page).to have_css('.state-1-table')
+    within ('.state-1-table') do
+      expect(page).to have_css('.aggravated-assault')
+      expect(page).to have_css('.homicide')
+      expect(page).to have_css('.rape')
+      expect(page).to have_css('.property-crime')
+      expect(page).to have_css('.arson')
+    end
+
+    expect(page).to have_css('.state-2-table')
+    within ('.state-2-table') do
+      expect(page).to have_css('.aggravated-assault')
+      expect(page).to have_css('.homicide')
+      expect(page).to have_css('.rape')
+      expect(page).to have_css('.property-crime')
+      expect(page).to have_css('.arson')
+    end
+  end
+end
